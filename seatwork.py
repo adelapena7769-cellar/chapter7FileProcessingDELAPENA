1216015fafb067629f5a549f52c1377b85e4130d
adelapena <adelapena7769@umak.edu.ph>
2026-09-21 19:37:50 +0800

    CHAPTER 7 warm-up 2.0

---
+++ seatwork.py
@@ -0,0 +1,16 @@
+file_name = input('Enter a File name: ')
+
+    if file_name == "done":
+        break
+
+    try:
+        file_name = string(file_name)
+
+    except:
+        print("Invalid File")
+
+
+
+
+
+
